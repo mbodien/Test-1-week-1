86c94c0481f1ee0de5a713c5cc84c2eec82eda9b
Matt <mbodien@gmail.com>
2017-05-21 17:47:48 +0100

    Test 2, sending from webstorm

---
+++ js/main.js
@@ -0,0 +1,28 @@
+$(document).ready(function () {
+    $('.page').hide();
+    $('#page1').show();
+    nextPage()
+})
+
+
+
+var currentPage = 1;
+
+function nextPage() {
+    var pageHide = '#page' + currentPage;
+    currentPage++;
+    var pageShow = '#page' + currentPage;
+    $(pageHide).hide();
+    $(pageShow).show();
+    console.log(currentPage, pageShow)
+}
+
+
+
+
+var select = '';
+for (i=18;i<=100;i++) {
+    select += '<option val=' + i + '>' + i + '</option>';
+}
+$('#18_100').html(select);
+
